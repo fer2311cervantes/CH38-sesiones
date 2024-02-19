0466f3841256ad0dcdeef0c1dfd1ce6aa60799f2
Fernando Cervantes <fer2311cervantes@gmail.com>
2024-02-19 13:28:36 -0600

    ci: cambios funciones de mensaje

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hola CH38");
+
+function enviarSaludo(){
+    alert("Hello");
+}
+
+/**
+ * pedir el nombre de la persona (prompt)
+ * Mandar saludo personalizado (alert)
+ */
+function greetWithName(){
+    const name = prompt("What is your name?");
+    alert(`Hi ${name}!`);
+}
