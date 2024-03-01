5e7389c7ad5e530e56995d207d774a5979306bc1
Fernando Cervantes <fer2311cervantes@gmail.com>
2024-03-01 10:56:45 -0600

    add: new folder for homework, the firt is web7

---
+++ assets/sesiones/web7/script.js
@@ -0,0 +1,46 @@
+console.log("Script de sesion Web07");
+
+// getElementById : seleccionar y obtener una referencia a un elemento HTML
+document.getElementById("title").innerHTML="El elote en CDMX cuesta $30";
+
+function handleTitle(){
+    document.getElementById("title").style.color = "blue";
+}
+function changeColor(color){
+    document.getElementById("description").style.color = color;
+}
+function changeColorTextUsingThis(color,elementRef){
+    /*there is no id */
+    elementRef.style.color = color;
+}
+
+/* Reuses code */
+function changeColorByCollection(collection,color){
+    for(index = 0; index < collection.length; index++){
+        collection[index].style.color = color;
+    }
+}
+
+function resetColorParagraph(){
+    const collection = document.getElementsByTagName("p");
+    changeColorByCollection(collection,"black");
+}
+
+function changeColorTitles(){
+    const collection = document.getElementsByClassName("title-article");
+    changeColorByCollection(collection,"#9FE2BF");
+}
+
+/* function changeColorTextUsingClass(){
+    const collection = document.getElementsByClassName("title-article");
+    for(index = 0; index < collection.length; index++){
+        collection[index].style.color = "#9FE2BF";
+    }
+}
+function changeColorTextUsingTag(){
+    const collection = document.getElementsByTagName("p");
+    for(index = 0; index < collection.length; index++){
+        collection[index].style.color = "black";
+    }
+} */
+
