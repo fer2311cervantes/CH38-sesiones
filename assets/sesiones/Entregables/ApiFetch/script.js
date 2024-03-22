6310de86b82c1ed657b9f169d0934646c1c57f28
Fernando Cervantes <fer2311cervantes@gmail.com>
2024-03-21 18:34:38 -0600

    fix: button only sets one timer

--- assets/sesiones/Entregables/ApiFetch/script.js
+++ assets/sesiones/Entregables/ApiFetch/script.js
@@ -1,4 +1,10 @@
 localStorage.clear(); //clean local storage every time I open the page
+
+/**
+ * builds the card list with users information
+ * @param {arr} users is an array of user info
+ * @returns arr of users cards
+ */
 const cards = (users) =>
     users.map( user => `
     <div class="card">
@@ -10,46 +16,79 @@ const cards = (users) =>
         </div>
     </div>
     ` );
-const printMessage = ( id , message ) => document.getElementById(id).innerHTML = message;
-const printCards = ( cards ) => document.getElementById("user-cards").innerHTML = cards.join("");
 
-let imTired = 0;
-const handleButton = () =>{
-    if( imTired === 0){   //I don´t know how to prevent to press the button many times :(
+/**
+ * prints a message in the element with the given id
+ * @param {string} id 
+ * @param {string} message 
+ * @returns nothing 
+ */
+const printMessage = ( id , message ) => document.getElementById(id).innerHTML = message;
+
+/**
+ *  prints a card list in the element with the given id
+ * @param {*} id 
+ * @param {*} cards 
+ * @returns 
+ */
+const printCards = ( id,cards ) => document.getElementById(id).innerHTML = cards.join(""); //id = "user-cards"
+
+/**
+ * gets users form local storage (key "user") or api when push button
+ */
+const handleButton = () =>{    
     const myURL = "https://reqres.in/api/users?delay=3"; //url with users info
     printMessage("user-cards", "");  //remove user cards every time I push the button
-
     localStorage.getItem("users") || printMessage("text-info", "Loading . . .");  //show message loading
     localStorage.getItem("users") && getUsersLS(); //if users exist in local storage get from there
     localStorage.getItem("users") || getUsersAPI(myURL); //if users does not exist in local storage get from API
-    
-    imTired = 1;
-    }
 }
+/**
+ * async fnc: gets users from given url, send users to local storage [ setLocalStorage() ] and print cards [ printCards() ]
+ * (id element "user-cards")
+ * @param {string} url 
+ */
 const getUsersAPI = async ( url ) => {
     const resolve = await fetch( url );
     const usersData = await resolve.json();
     const users = usersData.data;
-    //console.log("this shouldn't happend");
-    setLocalStorage(users);
-    printCards(cards(users)); //make and print cards
+    setLocalStorage(users); //set local storage with users list
+    printCards("user-cards",cards(users)); //make and print cards
 }
+
+let oneTime = true;   //this prevents to set more than 1 timer
+/**
+ * set users in local storage (key "user") and builds timers, needs a global boolean variable named oneTime to works (starts in true)
+ * @param {array} arr 
+ */
 const setLocalStorage = (arr) => {
     localStorage.setItem("users",JSON.stringify(arr));
-    createTime();
-    timerForLocalStorage();   
+    if(oneTime) {          
+        createTime();  //creates a new value for start counting for 1 minute
+        timerForLocalStorage();  //set a timer for print time untill delete local storage
+        oneTime = false;
+    }
 }
+/**
+ * get users from local storage (key "users") and print cards with them (id element "user-cards")
+ */
 const getUsersLS = () => {
     const recuperDatos = localStorage.getItem( "users" );
     const users = JSON.parse(recuperDatos) ;
-    printCards(cards(users)); //make and print cards
+    printCards("user-cards",cards(users)); //make and print cards
 }
-
+/**
+ * sets up a new date for start counting 1 minute
+ */
 const createTime = () => {
     const setUpTime = new Date().getTime();
     localStorage.setItem("setUpTime",setUpTime);
 }
 
+/**
+ * prints message (id "text-info") with remaining time in seconds if les than 1 minute from createTime(), 
+ * if more the 1 minute calls removeStorage()
+ */
 const timerForLocalStorage = () => {
     const intervalId = setInterval( 
       ()=> {
@@ -60,11 +99,14 @@ const timerForLocalStorage = () => {
         ( a < 1) && removeStorage(intervalId);
     }, 1000);
 }
-
+/**
+ * removes timers and local storage, cleans screen (id "user-cards" and "text-info")
+ * @param {interval id} intervalId 
+ */
 const removeStorage = (intervalId) => {
-    localStorage.clear();
-    printMessage( "text-info", "" );
-    printMessage("user-cards", "");
-    clearInterval(intervalId);
-    imTired = 0;
+    localStorage.clear();  //clean local storage
+    printMessage( "text-info", "" ); //clean screen
+    printMessage("user-cards", ""); //clean screen  
+    clearInterval(intervalId);   
+    oneTime = true;        //removes the prevention for timer
 }
